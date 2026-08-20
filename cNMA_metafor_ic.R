42ef5af6cbb0c1d2f894d5b1781c306bbc7484b5
Seth B Morgan <seth.b.morgan@gmail.com>
2026-08-20 17:00:44 -0400

    Update cNMA_metafor_ic.R

--- cNMA_metafor_ic.R
+++ cNMA_metafor_ic.R
@@ -198,14 +198,16 @@ CNMA_Data <- read_sheet("https://docs.google.com/spreadsheets/d/1oCcRHU6OSc64OWV
         ungroup()
     }
     
-    CNMA_Data <- make_indicator_labels_plus(CNMA_Data, c("NL_TX", "N_TX", "R_TX", "RV_TX", "ME_TX", "VT_TX", "WTS_TX", "SV_TX", "FF_TX", "FO_TX", "BR_TX", "MR_TX", "PREXTRA_TX", "BX_TX", "WXA_TX", "WXP_TX", "MS2_TX", "WPS_TX", "WP2_TX", "MS_TX", "BFS_TX"))
+    #CNMA_Data <- make_indicator_labels_plus(CNMA_Data, c("NL_TX", "N_TX", "R_TX", "RV_TX", "ME_TX", "VT_TX", "WTS_TX", "SV_TX", "FF_TX", "FO_TX", "BR_TX", "MR_TX", "PREXTRA_TX", "BX_TX", "WXA_TX", "WXP_TX", "MS2_TX", "WPS_TX", "WP2_TX", "BFS_TX")) #Per team guidance, exclude MS_TX in favor of MS2_TX.
+    CNMA_Data <- make_indicator_labels_plus(CNMA_Data, c("NL_TX", "R_TX", "ME_TX", "VT_TX", "WTS_TX", "FF_TX", "BR_TX", "MR_TX", "WXA_TX", "MS2_TX", "BFS_TX")) #This version only includes the componets we are entering as modertors in the CNMA fitted with the rma.mv() function below: NL + R + ME + VT + WTS + FF + BR + MR + WXA + MS2 + BFS
     tabyl(CNMA_Data$intervention_component_bundle)
     class(CNMA_Data$intervention_component_bundle)
     #CNMA_Data$intervention_component_bundle <- as.factor(CNMA_Data$intervention_component_bundle)
     class(CNMA_Data$intervention_component_bundle)
     tabyl(CNMA_Data$intervention_component_bundle)
-    CNMA_Data %>% count(intervention_component_bundle, NL_TX, N_TX, R_TX, RV_TX, ME_TX, VT_TX, WTS_TX, SV_TX, FF_TX, FO_TX, BR_TX, MR_TX, PREXTRA_TX, BX_TX, WXA_TX, WXP_TX, MS2_TX, WPS_TX, WP2_TX, MS_TX, BFS_TX) %>% print(n = Inf)
-
+    #CNMA_Data %>% count(intervention_component_bundle, NL_TX, N_TX, R_TX, RV_TX, ME_TX, VT_TX, WTS_TX, SV_TX, FF_TX, FO_TX, BR_TX, MR_TX, PREXTRA_TX, BX_TX, WXA_TX, WXP_TX, MS2_TX, WPS_TX, WP2_TX, BFS_TX) %>% print(n = Inf)
+    CNMA_Data %>% count(intervention_component_bundle, NL_TX, R_TX, ME_TX, VT_TX, WTS_TX, FF_TX, BR_TX, MR_TX, WXA_TX, MS2_TX, BFS_TX) %>% print(n = Inf)
+    
     ### comparison_component_bundle
     make_indicator_labels_plus <- function(df, cols, new_col = "comparison_component_bundle") {
       df %>%
@@ -218,14 +220,16 @@ CNMA_Data <- read_sheet("https://docs.google.com/spreadsheets/d/1oCcRHU6OSc64OWV
         ungroup()
     }
     
-    CNMA_Data <- make_indicator_labels_plus(CNMA_Data, c("NL_COMP", "N_COMP", "R_COMP", "RV_COMP", "ME_COMP", "VT_COMP", "WTS_COMP", "SV_COMP", "FF_COMP", "FO_COMP", "BR_COMP", "MR_COMP", "PREXTRA_COMP", "BX_COMP", "WXA_COMP", "WXP_COMP", "MS2_COMP", "WPS_COMP", "WP2_COMP", "MS_COMP", "BFS_COMP"))
+    #CNMA_Data <- make_indicator_labels_plus(CNMA_Data, c("NL_COMP", "N_COMP", "R_COMP", "RV_COMP", "ME_COMP", "VT_COMP", "WTS_COMP", "SV_COMP", "FF_COMP", "FO_COMP", "BR_COMP", "MR_COMP", "PREXTRA_COMP", "BX_COMP", "WXA_COMP", "WXP_COMP", "MS2_COMP", "WPS_COMP", "WP2_COMP", "BFS_COMP")) #Per team guidance, exclude MS_COMP in favor of MS2_COMP.
+    CNMA_Data <- make_indicator_labels_plus(CNMA_Data, c("NL_COMP", "R_COMP", "ME_COMP", "VT_COMP", "WTS_COMP", "FF_COMP", "BR_COMP", "MR_COMP", "WXA_COMP", "MS2_COMP", "BFS_COMP")) #This version only includes the componets we are entering as modertors in the CNMA fitted with the rma.mv() function below: NL + R + ME + VT + WTS + FF + BR + MR + WXA + MS2 + BFS
     CNMA_Data <- CNMA_Data %>% mutate(comparison_component_bundle = if_else(comparison_component_bundle == "", "BAU", comparison_component_bundle))
     tabyl(CNMA_Data$comparison_component_bundle)
     class(CNMA_Data$comparison_component_bundle)
     #CNMA_Data$comparison_component_bundle <- as.factor(CNMA_Data$comparison_component_bundle)
     class(CNMA_Data$comparison_component_bundle)
     tabyl(CNMA_Data$comparison_component_bundle)
-    CNMA_Data %>% count(comparison_component_bundle, NL_COMP, N_COMP, R_COMP, RV_COMP, ME_COMP, VT_COMP, WTS_COMP, SV_COMP, FF_COMP, FO_COMP, BR_COMP, MR_COMP, PREXTRA_COMP, BX_COMP, WXA_COMP, WXP_COMP, MS2_COMP, WPS_COMP, WP2_COMP, MS_COMP, BFS_COMP) %>% print(n = Inf)
+    #CNMA_Data %>% count(comparison_component_bundle, NL_COMP, N_COMP, R_COMP, RV_COMP, ME_COMP, VT_COMP, WTS_COMP, SV_COMP, FF_COMP, FO_COMP, BR_COMP, MR_COMP, PREXTRA_COMP, BX_COMP, WXA_COMP, WXP_COMP, MS2_COMP, WPS_COMP, WP2_COMP, BFS_COMP) %>% print(n = Inf)
+    CNMA_Data %>% count(intervention_component_bundle, NL_COMP, R_COMP, ME_COMP, VT_COMP, WTS_COMP, FF_COMP, BR_COMP, MR_COMP, WXA_COMP, MS2_COMP, BFS_COMP) %>% print(n = Inf)
     
   ## Create contrast codes   
   make_contrast_column <- function(df, col1, col2, new_col) {
@@ -304,19 +308,25 @@ CNMA_Data <- read_sheet("https://docs.google.com/spreadsheets/d/1oCcRHU6OSc64OWV
   CNMA_Data <- make_contrast_column(CNMA_Data, "BFS_TX", "BFS_COMP", BFS)
   CNMA_Data %>% count(BFS_TX, BFS_COMP, BFS) %>% print(n = Inf)  
   
-  ## Drop intervention versus comparison contrasts that have the same bundles
-  CNMA_Data <- CNMA_Data %>% mutate(nonzero_count = rowSums(across(c(NL, N, R, RV, ME, VT, WTS, SV, FF, FO, BR, MR, PREXTRA, BX, WXA, WXP, MS2, WPS, WP2, BFS), ~ .x != 0)))
-  CNMA_Data2_dropcheck <- CNMA_Data %>% dplyr::select(study_id, contrast_id, es_id, intervention_component_bundle, comparison_component_bundle, NL, N, R, RV, ME, VT, WTS, SV, FF, FO, BR, MR, PREXTRA, BX, WXA, WXP, MS2, WPS, WP2, BFS, nonzero_count)
-  print(CNMA_Data2_dropcheck, n=Inf)
-  #write_csv(CNMA_Data2_dropcheck, file = "CNMA_Data2_dropcheck.csv")
-  #write_xlsx(CNMA_Data2_dropcheck, 'CNMA_Data2_dropcheck.xlsx')
-  CNMA_Data %>% count()
-  CNMA_Data_nomirrors <- CNMA_Data %>% filter(!(nonzero_count == 0)) #Those rows with all zero values for the contrast-coded components have the exact same intervention/comparison component bundles and thus are "mirrors" which we are dropping from the analysis.
-  CNMA_Data_nomirrors %>% count()
+  ## Check for lone components and mirrored contrasts
+  # Note: Lone components are those that appear in a mirrored contrast and do not appear in any other T v BAU contrast.
+  #       Mirrored contrasts are those that have the exact same (bundle of) components on both sides of the contrast.
+  tabyl(CNMA_Data$intervention_component_bundle)
+  tabyl(CNMA_Data$comparison_component_bundle)
+  CNMA_Data %>% count(intervention_component_bundle, comparison_component_bundle) %>% print(n = Inf) 
   
-  ## Check for lone components
-  # Note: Lone components are checked for in the CNMA_Data2_dropcheck.csv file exported above by summing the absolute value of each contrast-coded component column. 
-  #       Any column summng to just ==1 would be a lone component. None do so there are no lone components in the CNMA database.
+  CNMA_Data <- CNMA_Data %>% mutate(intervention_component_bundle_clean = str_replace_all(intervention_component_bundle, "_TX", ""))
+  CNMA_Data <- CNMA_Data %>% mutate(comparison_component_bundle_clean = str_replace_all(comparison_component_bundle, "_COMP", ""))
+  tabyl(CNMA_Data$intervention_component_bundle_clean)
+  tabyl(CNMA_Data$comparison_component_bundle_clean)
+  CNMA_Data %>% count(intervention_component_bundle_clean, comparison_component_bundle_clean) %>% print(n = Inf)
+  
+  CNMA_Data <- CNMA_Data %>% mutate(mirrored_contrast = if_else(intervention_component_bundle_clean == comparison_component_bundle_clean, "YES", "NO"))
+  tabyl(CNMA_Data$mirrored_contrast)
+  CNMA_Data %>% count(mirrored_contrast, intervention_component_bundle_clean, comparison_component_bundle_clean) %>% print(n = Inf)
+  
+  ## Drop intervention versus comparison contrasts that have the same bundles
+
   
 # Execute additive component network meta-analysis using a contrast-based random-effects model using BAU as the reference condition: intervention_content == "Whole Numbers (W)"
       
